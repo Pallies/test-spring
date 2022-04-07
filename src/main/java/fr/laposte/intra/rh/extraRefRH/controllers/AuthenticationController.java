2715935f038b791930a0f6a8b6995ec043a64886
Rossi <rossi.oddet@dev-institut.fr>
2022-04-07 03:05:19 +0200

    demo spring security test

--- src/main/java/fr/laposte/intra/rh/extraRefRH/controllers/AuthenticationController.java
+++ src/main/java/fr/laposte/intra/rh/extraRefRH/controllers/AuthenticationController.java
@@ -1,10 +1,12 @@
 package fr.laposte.intra.rh.extraRefRH.controllers;
 
 
+import fr.laposte.intra.rh.extraRefRH.models.LoginUser;
 import fr.laposte.intra.rh.extraRefRH.services.AuthentificationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("authentication")
 public class AuthenticationController {
 
-@Autowired
+    @Autowired
     private AuthentificationService authentificationService;
 
+    // J'ai crée une structure LoginUser en entrée du contrôleur.
+    // Il n'est pas logique que pour s'authentifier on ait à fournir l'intégralité du contexte
     @PostMapping("/login")
-    public ResponseEntity<UserDetails> userLogin(@RequestBody UserDetails user) throws UsernameNotFoundException {
-        return ResponseEntity.ok().body(authentificationService.login(user));
+    public ResponseEntity<UserDetails> userLogin(@RequestBody LoginUser user, @AuthenticationPrincipal UserDetails userDetails) throws UsernameNotFoundException {
+
+        // Validation de l'injection de l'utilisateur connecté via @AuthenticationPrincipal
+        return ResponseEntity.ok().body(authentificationService.login(userDetails));
     }
 
 }
--- src/test/java/fr/laposte/intra/rh/extraRefRH/controllers/AuthenticationControllerTest.java
+++ src/test/java/fr/laposte/intra/rh/extraRefRH/controllers/AuthenticationControllerTest.java
@@ -1,33 +1,60 @@
 package fr.laposte.intra.rh.extraRefRH.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.laposte.intra.rh.extraRefRH.config.jwt.JwtAuthenticationEntryPoint;
 import fr.laposte.intra.rh.extraRefRH.services.AuthentificationService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
+import java.util.Map;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(AuthenticationController.class)
+@WebMvcTest(controllers = AuthenticationController.class)
+// @WebMvcTest inclut la configuration Spring Security
+// WebSecurityConfig requiert un bean de type AuthenticationEntryPoint.
+// @Import() complète la configuration du test en ajoutant les beans précisés
+@Import(JwtAuthenticationEntryPoint.class)
 class AuthenticationControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    AuthentificationService authentificationService;
+
+    @Autowired
+    ObjectMapper mapper;
+
     @Test
     @WithMockUser(username = "test",password = "tester")
     void userLogin() throws Exception {
 
-        mockMvc.perform(post("/authentication/login")).andExpect(status().isCreated());
+        // vérification que le test prend bien en compte la configuration Spring Security
+        // l'utilisateur est automatiquement valorisé
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // Définition du comportement du service
+        // ?? Curieux qu'il ait en paramètre UserDetails ??
+        Mockito.when(authentificationService.login(user)).thenReturn(user);
+
+        Map<String, String> body = Map.of("username", "test", "password", "pass");
+
+        // Requête /POST
+        mockMvc.perform(
+                post("/authentication/login")
+                       .header("Content-type", "application/json")
+                        .content(mapper.writeValueAsString(body)))
+                .andExpect(status().isOk());
     }
 
-    @Test
-    void userRegistration() {
-    }
 }
